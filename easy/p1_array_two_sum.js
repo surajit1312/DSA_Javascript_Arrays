6690cd4688dfc9f0830b29cb1f1b9af230ec8d42
surajitr1 <surajitr1@vmware.com>
2023-11-05 03:45:17 +0530

    feat[arrays]: two sum problem added

---
+++ easy/p1_array_two_sum.js
@@ -0,0 +1,48 @@
+const consoleUtils = require("console-log-pattern/src/index");
+
+consoleUtils.logTitle("Leetcode 1. Two Sum: Arrays", "◉", "green");
+consoleUtils.logExeBlock(
+  `Input: nums = [3, 2, 5, 7, 11, 15]; target = 9`,
+  true,
+  "magenta"
+);
+consoleUtils.logExeBlock(
+  `Link: https://leetcode.com/problems/two-sum/`,
+  true,
+  "magenta"
+);
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function (nums, target) {
+  return findTwoSumIndices(nums, target, []);
+};
+
+const findTwoSumIndices = function (nums, target, result) {
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    let remainder = target - nums[i];
+    if (!map[remainder]) {
+      map[nums[i]] = { val: nums[i], index: i };
+    } else {
+      result = [map[remainder].index, i];
+      break;
+    }
+  }
+  return result;
+};
+
+const nums = [3, 2, 5, 7, 11, 15],
+  target = 9;
+
+const output = twoSum(nums, target);
+consoleUtils.logConsole(
+  `Indices of numbers with two sum ${target}: ${output}`,
+  false,
+  "magenta"
+);
+
+consoleUtils.logExeBlock("", true, "magenta");
